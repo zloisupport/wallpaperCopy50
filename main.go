42ecfaba50f4f50ae3874d294dcc0c22a431ecfb
zloisupport <zlo1supp0rt@gmail.com>
2024-05-07 03:44:39 +0600

    add: set default random wallpaper

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -18,6 +20,10 @@ var (
 	systemParametersInfo = user32.NewProc("SystemParametersInfoW")
 )
 
+type Wallpapers struct {
+	name []string
+}
+
 const (
 	SPI_SETDESKWALLPAPER = 20
 	SPIF_UPDATEINIFILE   = 0x01
@@ -25,6 +31,11 @@ const (
 )
 
 func main() {
+	setWallpaper()
+	copyRandomWallpaper()
+}
+
+func copyRandomWallpaper() {
 
 	rand.Seed(time.Now().UnixNano())
 	wallpaperDir := "./wallpaper"
@@ -35,6 +46,7 @@ func main() {
 		os.Exit(1)
 	}
 	var newFiles []os.FileInfo
+
 	randomFiles := make([]os.FileInfo, len(files))
 
 	for _, file := range files {
@@ -42,17 +54,34 @@ func main() {
 			newFiles = append(newFiles, file)
 		}
 	}
-	randomIndex := rand.Perm(len(newFiles))
-	for i := 0; i < 50; i++ {
-		randomFiles[i] = newFiles[randomIndex[i]]
-	}
-	if len(randomFiles) > 50 {
-		randomFiles = randomFiles[:50]
+	if len(newFiles) >= 50 {
+		randomIndex := rand.Perm(len(newFiles))
+		for i := 0; i < 50; i++ {
+			randomFiles[i] = newFiles[randomIndex[i]]
+		}
+		if len(randomFiles) > 50 {
+			randomFiles = randomFiles[:50]
+		}
+
+		randomFile := randomFiles[rand.Intn(len(randomFiles))]
+		fmt.Println(randomFile)
+
+		picturesDir := pictureFolder()
+
+		for _, file := range randomFiles {
+			srcPath := filepath.Join(wallpaperDir, file.Name())
+
+			dstPath := filepath.Join(picturesDir, file.Name())
+			if err := copyFile(srcPath, dstPath); err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+		}
+		//setDefaultWallpaper(picturesDir, randomFile)
 	}
 
-	randomFile := randomFiles[rand.Intn(len(randomFiles))]
-	fmt.Println(randomFile)
-
+}
+func pictureFolder() string {
 	userDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -66,24 +95,114 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	return picturesDir
+}
 
-	for _, file := range randomFiles {
-		srcPath := filepath.Join(wallpaperDir, file.Name())
+func copyDefaultWallpapers(files []string) {
+	pictureFolder := pictureFolder()
 
-		dstPath := filepath.Join(picturesDir, file.Name())
-		if err := copyFile(srcPath, dstPath); err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-
+	for _, file := range files {
+		srcPath := filepath.Join("./wallpaper", file)
+		dstPath := filepath.Join(pictureFolder, file)
+		if checkFileS(dstPath) {
+			if err := copyFile(srcPath, dstPath); err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
 		}
 	}
-	wallpaperPath := syscall.StringToUTF16Ptr(filepath.Join(picturesDir, randomFile.Name()))
-	result, _, _ := systemParametersInfo.Call(uintptr(SPI_SETDESKWALLPAPER), 0, uintptr(unsafe.Pointer(wallpaperPath)), uintptr(SPIF_UPDATEINIFILE|SPIF_SENDCHANGE))
-	if result != 0 {
-		fmt.Println("Wallpaper set")
-	} else {
-		fmt.Println("Ops, code:", result)
+}
+
+func setWallpaper() {
+
+	file := openDefaultJson()
+	copyDefaultWallpapers(file)
+	wall := selectWallpaper(file)
+	fStatus := checkFile(wall)
+	if fStatus {
+		setDefaultWallpaper(pictureFolder(), wall)
 	}
+}
+
+func checkFile(file os.FileInfo) bool {
+	if _, err := os.Stat(file.Name()); err == nil {
+		return true
+	}
+	return false
+}
+
+func checkFileS(file string) bool {
+	if _, err := os.Stat(file); err == nil {
+		return true
+	}
+	return false
+}
+
+func setDefaultWallpaper(picturesDir string, randomFile os.FileInfo) {
+	if len(picturesDir) != 0 {
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		wallpaperPath := syscall.StringToUTF16Ptr(filepath.Join(dir, randomFile.Name()))
+		result, _, _ := systemParametersInfo.Call(uintptr(SPI_SETDESKWALLPAPER), 0, uintptr(unsafe.Pointer(wallpaperPath)), uintptr(SPIF_UPDATEINIFILE|SPIF_SENDCHANGE))
+		if result != 0 {
+			fmt.Println("Wallpaper set")
+		} else {
+			fmt.Println("Ops, code:", result)
+		}
+	}
+}
+
+func selectWallpaper(files []string) os.FileInfo {
+	wallpapers := Wallpapers{name: files}
+	randomFile := rand.Intn(len(wallpapers.name))
+
+	fileInfo, err := os.Stat(wallpapers.name[randomFile])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileInfo
+}
+
+func openDefaultJson() []string {
+	if !checkFileS("wallpaper.json") {
+		createDefaultJson()
+	}
+	f, err := os.Open("wallpaper.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wallpapers []string
+	err = json.NewDecoder(f).Decode(&wallpapers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return wallpapers
+}
+
+func createDefaultJson() {
+	wallpaper := Wallpapers{name: []string{"1.jpg", "2.png", "3.jpg"}}
+	fmt.Println(wallpaper)
+	f, err := os.OpenFile("wallpaper.json", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	jsonBytes, err := json.MarshalIndent(wallpaper.name, "", "   ")
+	if err != nil {
+		fmt.Println("Error Marshal Parsing!:", err)
+		return
+	}
+
+	_, err = f.Write(jsonBytes)
+	if err != nil {
+		fmt.Println("Error Writing to File!:", err)
+		return
+	}
+	fmt.Println("Data successfully written to wallpaper.json")
 }
 
 func copyFile(src string, dst string) error {
